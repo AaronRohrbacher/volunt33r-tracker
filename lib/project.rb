7b9bbed61e21990ae3a461b003181b4e00e93428
Aaron Rohrbacher <arjazz@icloud.com>
2017-09-22 10:53:24 -0700

    pass #volunteers spec, returning list of volunteers for a particular project

--- lib/project.rb
+++ lib/project.rb
@@ -25,12 +25,23 @@ class Project
   end
 
   def self.find(id)
-    returned_projects = DB.exec("SELECT * FROM projects WHERE id = #{id}")
+    returned_projects = DB.exec("SELECT * FROM projects WHERE id = #{id};")
     projects = []
     returned_projects.each() do |project|
       title = project["title"]
       projects.push(Project.new({:title => title}))
     end
     projects[0]
+  end
+
+  def volunteers
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id};")
+    project_volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer["name"]
+      project_id = volunteer["project_id"].to_i
+      project_volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    project_volunteers
   end
 end
---
+++ lib/volunteer.rb
@@ -0,0 +1,39 @@
+class Volunteer
+  attr_reader(:id, :project_id, :name)
+  def initialize(attributes)
+    @name = attributes[:name]
+    @project_id = attributes[:project_id]
+    @id = attributes[:id]
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result[0]["id"].to_i
+  end
+
+  def ==(another_volunteer)
+    self.name().==(another_volunteer.name())
+  end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_vonlunteers.each() do |volunteer|
+      name = volunteer["name"]
+      project_id = volunteer["project_id"]
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id}))
+    end
+    volunteers
+  end
+
+  # def self.find(id)
+  #   returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE id = #{id};")
+  #   volunteers = []
+  #   returned_volunteers.each() do |volunteer|
+  #     name = volunteer["name"]
+  #     volunteers.push(Volunteer.new({:name => name}))
+  #   end
+  #   volunteers
+  # end
+
+end
